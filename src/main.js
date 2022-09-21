50f0b58f95f8fce8b34505913b26e8d5abd0c764
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2022-09-21 18:00:28 +0800

    数据传输成功

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { Client } from '@notionhq/client'
+import { KintoneRestAPIClient } from '@kintone/rest-api-client'
 
 const notion = new Client({ auth: 'secret_CNqoK82a5MQaBosS6jMEchHbQhX2806zNljRgShGuK' })
-
-// const urlQueryDB = 'https://api.notion.com/v1/databases/b269009a9a44488d9ff7fe0c646179c9/query'
-// const urlRetrieveSinglePage = 'https://api.notion.com/v1/pages/'
 
 const queryDatabase = async (dbid) => {
   const response = await notion.databases.query({
@@ -34,22 +32,35 @@ const iterateDB = async () => {
           properties[propertyName] = propertyData
           // console.log(properties)
           if (properties[propertyName].object === 'list') {
-            // console.log(properties[propertyName].results)
             // console.log(properties[propertyName].results[0])
             if (properties[propertyName].results[0].type === 'title') {
               // console.log(properties[propertyName].results[0][properties[propertyName].results[0].id].plain_text)
             }
           }
-          if (propertyName === 'Name') {
+          if (propertyName === '书名') {
             // console.log(properties.Name.results[0].title.plain_text)
-            return { Name: properties.Name.results[0].title.plain_text }
+            return { book_name: properties.书名.results[0].title.plain_text }
           }
-          if (propertyName === 'sele') {
-            // console.log(properties.sele.select.name)
-            return { Tags: properties.sele.select.name }
+          if (propertyName === '作者') {
+            // console.log(properties.作者.results[0].rich_text.plain_text)
+            return { author: properties.作者.results[0].rich_text.plain_text }
+          }
+          if (propertyName === '定价') {
+            // console.log(properties.定价)
+            return { price: properties.定价.number }
+          }
+          if (propertyName === '标签') {
+            return {
+              label: properties.标签.multi_select.map((m) => {
+                return m.name
+              }),
+            }
+            // for (const label of properties.标签.multi_select) {
+            //   console.log(label.name)
+            // }
+            // return { 作者: properties.作者.results[0].rich_text.plain_text }
           }
           // return properties
-          // return {}
         }),
       )
       return { ...record }
@@ -64,15 +75,30 @@ const preResult = await iterateDB()
 for (const record of preResult.records) {
   for (const [key, value] of Object.entries(record)) {
     // console.log(value)
-    if (value && value.Name) {
-      // console.log(value.Name)
-      record.Name = { value: value.Name }
+    if (value && value.book_name) {
+      record.book_name = { value: value.book_name }
     }
-    if (value && value.Tags) {
-      // console.log(value.Name)
-      record.Tags = { value: value.Tags }
+    if (value && value.author) {
+      record.author = { value: value.author }
+    }
+    if (value && value.price) {
+      record.price = { value: value.price }
+    }
+    if (value && value.label) {
+      record.label = { value: value.label }
     }
     delete record[key]
   }
 }
+preResult.app = 43
 console.log(preResult.records)
+
+const client = new KintoneRestAPIClient({
+  baseUrl: 'https://cndevqpofif.cybozu.cn',
+  auth: {
+    apiToken: 'WKuCUxqW8gRYrgs57w5UVAU9GUKYPbTU210MJ0bx',
+  },
+})
+
+const addedToKintone = await client.record.addRecords(preResult)
+console.log(addedToKintone)
